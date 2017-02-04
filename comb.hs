b3771984e6abdef5c65955c5efc47177c2a5fe78
dpiponi <dpiponi@gmail.com>
2017-02-03 18:00:34 -0800

    fexp implemented

--- comb.hs
+++ comb.hs
@@ -1,4 +1,5 @@
 import Data.Ratio
+import Debug.Trace
 
 sample a = map (flip count a) [0..10]
 
@@ -41,6 +42,7 @@ instance (Eq r, Num r) => Num [r] where
 instance (Eq r, Fractional r) => Fractional [r] where
     x/y = x ^/ y
     fromRational x    = fromRational x:repeat 0
+
 sqrt' x = 1:rs where rs = map (/2) (xs ^- (rs `convolve` (0:rs)))
                      _:xs = x
 instance (Eq r, Fractional r) => Floating [r] where
@@ -93,3 +95,46 @@ tree x = p where p = [0] ... union (set p) x
 graph = [2^((n*(n-1) `div` 2)) / product (map fromInteger [1..n]) | n <- [0..]] :: [Rational]
 
 connectedGraph = 1 + log graph
+
+delta (g : gs) h = let g' = delta gs h
+                   in (0 : ((1 : h) * g')) + gs
+
+fsqrt (0 : 1 : fs) =
+    let gs = (fs-(0 : gs*((0 : delta gs gs)+((2 : gs)*(gs*g)))))/2
+        g = 0 : 1 : gs
+    in g
+
+p f t = (t `compose` f) ^- t
+flog :: (Eq a, Fractional a) => [a] -> [a]
+flog f@(0 : 1 : _) = flog' f 1 (repeat 0) (0 : 1 : repeat 0)
+         where flog' f n t z = take (n+1) t ++ (
+                 drop (n+1) $
+                    let pz = p f z
+                    in flog' f (n+1) (t ^- map (((-1)^n / fromIntegral n) *) pz) pz)
+
+cbrt x = exp (map (/ 3) $ log x)
+
+fexp f@(0 : 0 : _) = fexp' f 0 t' 1
+fexp' f total term n = take (n-1) total ++ drop (n-1) (fexp' f (total+term) (map (/fromIntegral n) (f*d term)) (n+1))
+
+main = do
+    mapM_ print $ take 20 $ fexp (-flog (sin t'))
+    mapM_ print $ take 20 $ asin t'
+    print "--"
+    mapM_ print $ take 200 $ fexp (-flog (t'*exp t'))
+    --mapM_ print $ take 10 $ fexp (2*t'^2)
+{-
+    -- Compare http://math.stackexchange.com/questions/208996/half-iterate-of-x2c
+    mapM_ print $ take 20 $ flog (t'+t'^2)
+    print "---"
+    -- http://math.stackexchange.com/questions/1346667/lim-limits-x-rightarrow-0-frac-sin-nx-x-fracn6x3-left-frac
+    mapM_ print $ take 20 $ flog $ sin t'
+    print "---"
+    -- See bottom of page 14 http://www.springer.com/cda/content/document/cda_downloaddocument/9783642004490-c1.pdf?SGWID=0-0-45-767218-p173888310
+    -- or top of page 8 https://www.itp.uni-hannover.de/~flohr/papers/w-cft-survivalkit.pdf
+    mapM_ print $ take 20 $ flog (t'/(1-t'))
+    print "---"
+    mapM_ print $ take 20 $ flog $ t'/sqrt(1-2*t'^2)
+    print "---"
+    mapM_ print $ take 20 $ flog $ t'/cbrt(1-3*t'^3)
+    -}
